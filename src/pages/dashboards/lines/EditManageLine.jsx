71b7dcf418e2f043f34113d3452456261a5f4c85
surya-032 <114859751+surya-032@users.noreply.github.com>
2025-04-18 22:01:40 +0800

    Update EditManageLine.jsx

--- src/pages/dashboards/lines/EditManageLine.jsx
+++ src/pages/dashboards/lines/EditManageLine.jsx
@@ -32,7 +32,7 @@ export default function EditManageLine() {
   };
 
   useEffect(() => {
-    fetch("https://gis-backend-production-f4bc.up.railway.app/routes")
+    fetch("/api/routes")
       .then(response => response.json())
       .then(data => setRoutes(data))
       .catch(error => console.error("Error fetching routes:", error));
